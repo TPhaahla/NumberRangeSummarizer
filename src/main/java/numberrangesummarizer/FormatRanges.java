01cf6101c00c690ac5c8c46a3c153ef4b1439853
Tshiamo Phaahla <72293519+TPhaahla@users.noreply.github.com>
2024-06-11 19:35:24 +0200

    Make collect() and summarizeCollection() more robust - Error handling - Input preprocessing (remove duplicates and presort) - ensure last range is added. - Edit return type format
    Signed-off-by: Tshiamo Phaahla <72293519+TPhaahla@users.noreply.github.com>

--- src/main/java/numberrangesummarizer/FormatRanges.java
+++ src/main/java/numberrangesummarizer/FormatRanges.java
@@ -2,20 +2,23 @@ package numberrangesummarizer;
 
 import java.util.*;
 
-public class FormatRanges implements NumberRangeSummarizer{
+public class FormatRanges implements NumberRangeSummarizer {
     @Override
-    public Collection<Integer> collect(String input){
+    public Collection<Integer> collect(String input) {
         Collection<Integer> numbers = new ArrayList<>();
 
-        if (input!=null && !input.isEmpty()){
+        if (input != null && !input.isEmpty()) {
+            if (input.matches(".*\\s+.*") || input.contains("\t")) {
+                throw new IllegalArgumentException(
+                        "Invalid input: Input should be comma delimited with no whitespace characters.");
+            }
+
             Scanner scanner = new Scanner(input).useDelimiter(",");
-            while (scanner.hasNext()){
-                if(scanner.hasNextInt()){
-//                    System.out.println(scanner.nextInt());
+            while (scanner.hasNext()) {
+                if (scanner.hasNextInt()) {
                     numbers.add(scanner.nextInt());
-                }
-                else {
-                    System.err.println("Invalid Number: " + scanner.next());
+                } else {
+                    throw new IllegalArgumentException("Invalid Number: " + scanner.next());
                 }
             }
             scanner.close();
@@ -26,39 +29,43 @@ public class FormatRanges implements NumberRangeSummarizer{
     @Override
     public String summarizeCollection(Collection<Integer> input) {
 
-        if (input== null || input.isEmpty()) {
+        if (input == null || input.isEmpty()) {
             return "";
         }
 
-        List<Integer> numbers = new ArrayList<>(input);
+
+//       Use HashSet to remove duplicates
+        Set<Integer> unique_input = new HashSet<>(input);
+        List<Integer> numbers = new ArrayList<>(unique_input);
+
+        // Presort the list
+        Collections.sort(numbers);
 
         List<String> summary = new ArrayList<>();
         int first_pointer = numbers.getFirst();
         int second_pointer = first_pointer;
 
-        for(int i=1; i<numbers.size(); i++){
+        for (int i = 1; i < numbers.size(); i++) {
             int current_reference = numbers.get(i);
 
-            if(current_reference == second_pointer + 1) {
+            if (current_reference == second_pointer + 1) {
                 second_pointer = current_reference;
-            }
-            else {
+            } else {
                 addRange(summary, first_pointer, second_pointer);
                 first_pointer = current_reference;
                 second_pointer = first_pointer;
             }
         }
-        return summary.toString();
+        addRange(summary, first_pointer, second_pointer);
+        return String.join(", ", summary);
     }
 
-    private void addRange(List<String> summary, int start, int end){
-        if(start==end){
+    private void addRange(List<String> summary, int start, int end) {
+        if (start == end) {
             summary.add(String.valueOf(start));
-        }
-        else{
+        } else {
             summary.add(String.valueOf(start) + "-" + String.valueOf(end));
         }
     }
-
 
 }
